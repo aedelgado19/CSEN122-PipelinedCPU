e33f587b7aa70931eb6559b1075d456f31210d1e
Ally Delgado <61644740+aedelgado19@users.noreply.github.com>
2025-06-08 15:57:23 -0700

    compiling pipelined_datapath.v

--- pipelined_datapath.v
+++ pipelined_datapath.v
@@ -1,10 +1,12 @@
 //when we say like ifid_blah, that means the blah LEAVES the ifid buffer (not enters)
 
 module pipelined_datapath(input clk);
-	
+	// making these first so the file compiles:
+	//MEM/WB buffer inputs are exmem_ALU_result, data_mem_out, and exmem_ctrl
+	wire [5:0] memwb_rd;
+	wire [31:0] wb_data;
+	wire [6:0] memwb_ctrl;
 //===================== Stage 1 (IF stage) =====================
-	
-	//remember to instantiate the mux that has 3 inputs: PC+1, jump, and rs (from ALU), and outputs pc_in !!!!!!!!!!!!!!
 	//PC Sel is created in EXMEM stage
 
 	// pc
@@ -13,18 +15,17 @@ module pipelined_datapath(input clk);
 	// pc_adder inputs are the PC_out, 1, and outputs pc_plus1
 	wire [31:0] pc_plus1;
 
-
 	// inst_mem takes in PC_out amd outputs inst_out
 	wire [31:0] inst_out;
 	
-	// IF/ID buffer takes in PC, and inst out, and outputs ifid_pc and ifid_inst
-	wire [31:0] ifid_pc, ifid_inst;	
+	// IF/ID buffer takes in PC, and inst out, and outputs ifid_pc and ifid_instr
+	wire [31:0] ifid_pc, ifid_instr;	
 	
 
 // Instatiate stage 1 (IF stage) modules
 	mux3to1 pc_mux(
 		.a(pc_plus1), 
-		.b(ALU_result),
+		.b(alu_result),
 		.c(mem_data_out),
 		.sel1(pc_sel),  	//pc_sel from mem stage (stage 4), the or and and gates
 		.sel2(memwb_ctrl[0]),	//jump_mem control signal
@@ -42,7 +43,7 @@ module pipelined_datapath(input clk);
 	);
 	ALU pc_adder(
 		.A(pc_out), .B(32'd1), 
-		.aluOp(3'b111), //111 for add 
+		.add(1'b0), .inc(1'b1), .neg(1'b0), .sub(1'b0),
 		.out(pc_plus1), .Z(), .N()
 	);
 	ifid_buf ific(
@@ -50,7 +51,7 @@ module pipelined_datapath(input clk);
 		.instr_in(inst_out),
 		.pc_in(pc_out),
 		
-		.instr_out(ifid_inst),
+		.instr_out(ifid_instr),
 		.pc_out(ifid_pc)
 	);
 	
@@ -58,7 +59,8 @@ module pipelined_datapath(input clk);
 //===================== Stage 2 (ID stage) wires and regs =====================
  	// reg file
 	wire [5:0] rd_in, rs_in, rt_in; //rd_in/memwb_rd comes from writeback out of the MEM/WB buffer
-	wire [31:0] write_data, regW; //write data is the data to write, regW comes from stage 5
+	wire [31:0] write_data;
+	wire regW; //write data is the data to write, regW comes from stage 5
 	wire [31:0] rs_out, rt_out;
 
 	// imm gen
@@ -75,7 +77,7 @@ module pipelined_datapath(input clk);
 	// ID/EX buffer outputs
 	wire [31:0] idex_rs, idex_rt, idex_imm, idex_pc;
 	wire [5:0] idex_rd;
-	wire [8:0] idex_ctrl;
+	wire [11:0] idex_ctrl;
 	
 
 // Instantiate stage 2 (ID stage) modules
@@ -95,7 +97,7 @@ module pipelined_datapath(input clk);
 	
 	imm_gen imm_gen(
 		.clk(clk), 
-		.in(ifid_inst), 
+		.in(ifid_instr), 
 		.out(imm_out)
 	);
 	
@@ -126,8 +128,12 @@ module pipelined_datapath(input clk);
 	// ex_alu takes in: idex_rs, imm_mux_choice, idex_ALU_OP, and outputs Z, N, and 32bit ALU_result
 	wire [31:0] imm_mux_choice, alu_result, pc_branch; //imm_mux_choice is the result of the mux that chooses imm or rt
 	
+	//ALUOp
+	wire ex_add, ex_inc, ex_neg, ex_sub;
+	
 
-	//dont forget to instantiate the imm_mux
+	//select between imm or rt mux (0 = rt and 1 = imm)
+	wire ALUsrc;
 	
 	// EX/MEM buffer takes Zflag, Nflag, alu_result, idex_rt, PC + imm, idex_rd, and idex_ctrl except ALU_OP is dc
 	wire Zflag, Nflag;
@@ -139,83 +145,99 @@ module pipelined_datapath(input clk);
 	wire [5:0] exmem_rd;
 	wire [8:0] exmem_ctrl;
 
-// Instantiate stage 3 (EX stage) modules
+
+
+	//separate EX/MEM control wires
+	wire [2:0] exmem_aluOp;
+	wire exmem_memRead, exmem_memWrite, exmem_regWrite;
+	wire exmem_memToReg, exmem_pcToReg, exmem_brZ, exmem_brN;
+	wire exmem_jump, exmem_jump_mem;
+
+
+
+	//decode idex_ctrl into ALU signals
+	assign ex_add = (idex_ctrl[11:9] == 3'b000);
+	assign ex_sub = (idex_ctrl[11:9] == 3'b001);
+	assign ex_inc = (idex_ctrl[11:9] == 3'b010);
+	assign ex_neg = (idex_ctrl[11:9] == 3'b011);
+	
+	// Instantiate stage 3 (EX stage) modules
 	ALU ex_alu(
 		.A(idex_rs), .B(imm_mux_choice), 
-		.aluOp(ctrl[11:9]),
+		.add(ex_add), .inc(ex_inc), .neg(ex_neg), .sub(ex_sub),
 		.out(alu_result), .Z(Zflag), .N(Nflag)
 	);
 	mux2to1 alu_src_mux(
 		.a(idex_rt), 
 		.b(idex_imm), 
-		.sel(ctrl_out[?]), //ctrl indexed according to order specified above or in ctrl.v
+		.sel(ALUsrc), //ctrl indexed according to order specified above or in ctrl.v
 		.out(imm_mux_choice)
 	);
 	ALU ex_adder(
 		.A(idex_pc), .B(idex_imm),
-		.aluOp(3'b111),
+		.add(1'b1), .inc(1'b0), .neg(1'b0), .sub(1'b0),
 		.out(pc_plus_imm), .Z(), .N()
 	);
 	
-	exmem_buf exmem2(
-		.clk(clk),
-		.Z_in(Zflag).
-		.N_in(Nflag),
-		.alu_result_in(alu_result),
-		.rt_in(idex_rt),
-		.rd_in(idex_rd),
-		.pc_plus_imm_in(pc_plus_imm),
-		.aluOp_in(idex_ctrl[11:9]),
-		.MemRead_in(idex_ctrl[8]),
-		.MemWrite_in(idex_ctrl[7]),
-		.RegWrite_in(idex_ctrl[6]),
-		.MemToReg_in(idex_ctrl[5]),
-		.PCtoReg_in(idex_ctrl[4]),
-		.BrZ_in(idex_ctrl[3]),
-		.BrN_in(idex_ctrl[2]),
-		.jump_in(idex_ctrl[1]),
-		.jump_mem_in(idex_ctrl[0])
-		
-		.Z_out(Zflag),
-		.N_out(Nflag),
-		.alu_result_out(exmem_alu_result),
-		.rt_out(exmem_rt),
-		.rd_out(exmem_rd),
-		.pc_plus_imm_out(exmem_pc_plus_imm)
-		.aluOp_out(exmem_ctrl[11:9]),
-		.MemRead_out(exmem_ctrl[8]),
-		.MemWrite_out(exmem_ctrl[7]),
-		.RegWrite_out(exmem_ctrl[6]),
-		.MemToReg_out(exmem_ctrl[5]),
-		.PCtoReg_out(exmem_ctrl[4]),
-		.BrZ_out(exmem_ctrl[3]),
-		.BrN_out(exmem_ctrl[2]),
-		.jump_out(exmem_ctrl[1]),
-		.jump_mem_out(exmem_ctrl[0])
 
+	exmem_buf exmem(
+	    .clk(clk),
+	    //inputs
+	    .Z_in(Zflag),
+	    .N_in(Nflag),
+	    .alu_result_in(alu_result),
+	    .rt_in(idex_rt),
+	    .rd_in(idex_rd),
+	    .pc_plus_imm_in(pc_plus_imm),
+	    .aluOp_in(idex_ctrl[11:9]),
+	    .MemRead_in(idex_ctrl[8]),
+	    .MemWrite_in(idex_ctrl[7]),
+	    .RegWrite_in(idex_ctrl[6]),
+	    .MemToReg_in(idex_ctrl[5]),
+	    .PCtoReg_in(idex_ctrl[4]),
+	    .BrZ_in(idex_ctrl[3]),
+	    .BrN_in(idex_ctrl[2]),
+	    .jump_in(idex_ctrl[1]),
+	    .jump_mem_in(idex_ctrl[0]),
+
+	    //outputs
+	    .Z_out(Zflag),
+	    .N_out(Nflag),
+	    .alu_result_out(exmem_alu_result),
+	    .rt_out(exmem_rt),
+	    .rd_out(exmem_rd),
+	    .pc_plus_imm_out(exmem_pc_plus_imm),
+	    .aluOp_out(exmem_aluOp),
+	    .MemRead_out(exmem_memRead),
+	    .MemWrite_out(exmem_memWrite),
+	    .RegWrite_out(exmem_regWrite),
+	    .MemToReg_out(exmem_memToReg),
+	    .PCtoReg_out(exmem_pcToReg),
+	    .BrZ_out(exmem_brZ),
+	    .BrN_out(exmem_brN),
+	    .jump_out(exmem_jump),
+	    .jump_mem_out(exmem_jump_mem)
 	);
+
 	
 	
 
 //===================== Stage 4 (MEM stage) wires and regs =====================
-	// data_mem takes in idex_rt, ALU_result, exmem_ctrl[x] for MemR and exmem_ctrl[2] for MemW
-	wire mem_data_out;
+	
 
 	//intermediate stuff
+	wire [31:0] mem_data_out;
 	//instantiate the OR gate to choose between Z and N, and one more OR to choose between BrZ and BrN. 
 	//or gate inputs are zflag and nflag from ex/mem
-	wire or_alu_flags_out
+	wire or_alu_flags_out;
 	
 	//second or gate inputs are BrZ and BrN from control unit
 	wire or_branch_signals_out;
 	
 	
 	//Then the results of these go to an AND, and the output of the AND is the select line for the PC mux
-	wire pc_sel;
-	
-	// MEM/WB buffer inputs are exmem_ALU_result, data_mem_out, and exmem_ctrl
-	wire [5:0] memwb_rd;
 
+	// data_mem takes in idex_rt, ALU_result, exmem_ctrl[x] for MemR and exmem_ctrl[2] for MemW
 	wire [31:0] memwb_data_out; //this is the jmp signal 
 	wire [31:0] memwb_alu_result; 
 	
@@ -224,7 +246,7 @@ module pipelined_datapath(input clk);
 		.clk(clk), 
 		.r(exmem_ctrl[7]),  //MemRead
 		.w(exmem_ctrl[6]),  //MemWrite
-		.addr(exmem_alu_result_out), 
+		.addr(exmem_alu_result), 
 		.data_in(exmem_rt), 
 		.data_out(mem_data_out)
 	);
@@ -235,7 +257,7 @@ module pipelined_datapath(input clk);
 
 	assign pc_sel = or_alu_flags_out && or_branch_signals_out;
 	
-	memwb_buf memwb2(
+	memwb_buf memwb(
 		.clk(clk),
 		.data_in(mem_data_out),
 		.alu_result_in(exmem_alu_result),
@@ -250,7 +272,7 @@ module pipelined_datapath(input clk);
 		
 		.data_out(memwb_data_out),
 		.alu_result_out(memwb_alu_result),
-		.rd_out(memwb_rd)
+		.rd_out(memwb_rd),
 		.RegWrite_out(memwb_ctrl[6]),
 		.MemToReg_out(memwb_ctrl[5]),
 		.PCtoReg_out(memwb_ctrl[4]),
@@ -263,7 +285,7 @@ module pipelined_datapath(input clk);
 	
 
 //===================== Stage 5 (WB stage) wires and regs =====================
-	wire [31:0] wb_data;
+	
 	
 	mux3to1 wb_mux(
 		.a(pc_plus_imm),
