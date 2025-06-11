422678b714cc7ddb80e244316a0d1a92a56d04cd
Yi Qian Goh <112832226+yqgg@users.noreply.github.com>
2025-06-11 16:39:20 -0700

    Update buffers.v

--- buffers.v
+++ buffers.v
@@ -69,24 +69,24 @@ endmodule
 
 module exmemwb_buf(
     input clk,
-    input [31:0] pc_in,
     input [5:0] rd_in,
     input [31:0] data_in,
     input [31:0] alu_result_in,
     input MemToReg_in,
+    input PCtoReg_in,
     
-    output reg [31:0] pc_out,
     output reg [5:0] rd_out,
     output reg [31:0] data_out,
     output reg [31:0] alu_result_out,
-    output reg MemToReg_out
+    output reg MemToReg_out,
+    output reg PCtoReg_out
 );
 always @(posedge clk) begin
-        pc_out <= pc_in;
         rd_out <= rd_in;
         data_out <= data_in;
         alu_result_out <= alu_result_in;
         MemToReg_out <= MemToReg_in;
+        PCtoReg_out <= PCtoReg_in;
     end
 endmodule
 
