50f9f1b54762a882acf1d93c555f6c7fd9b703a0
Yi Qian Goh <112832226+yqgg@users.noreply.github.com>
2025-06-11 16:40:06 -0700

    Update muxes.v

--- muxes.v
+++ muxes.v
@@ -9,3 +9,22 @@ module mux2to1 #( parameter inputBitSize=32) (  //default size of 32 bits
 	    out = (sel == 0) ? a : b;
 	end
 endmodule
+
+module mux3to1(a, b, c, sel1, sel2, out);
+	input [31:0] a, b, c;
+	input sel1, sel2;
+	output reg [31:0] out;
+
+	wire [1:0] sel;
+	assign sel = {sel1, sel2};
+
+	always@(*) begin
+		case (sel)
+			2'b00: out = a;
+			2'b01: out = b;
+			2'b10: out = c;
+			default: out = 1'b0;
+		endcase
+	end
+
+endmodule
